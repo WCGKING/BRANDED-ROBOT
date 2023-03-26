e8bcab98beafa220a0dcd390f28e53efb9bdd859
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-03-27 01:59:01 +0530

    Update ping.py

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -40,13 +40,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
+    message = msg.reply_text("🐥 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "🚩ᴊᴀɪ sʜʀᴇᴇ ʀᴀᴍ 🚩 \n"
+        "✨❣️ᴊᴀɪ sʜʀᴇᴇ ʀᴀᴍ ❣️✨ \n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
