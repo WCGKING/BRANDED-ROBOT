727270485e6d0860dd681ced50380c1322b1c3cb
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-03-26 23:55:47 +0530

    Update ping.py

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -46,7 +46,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
+        "🚩ᴊᴀɪ sʜʀᴇᴇ ʀᴀᴍ 🚩 \n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
