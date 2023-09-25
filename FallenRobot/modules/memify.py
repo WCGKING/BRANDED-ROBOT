0df3c21fdb596d2163570bb73d54f9af262ddf46
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-25 12:32:27 +0530

    BRANDED ROBOT

--- FallenRobot/modules/memify.py
+++ FallenRobot/modules/memify.py
@@ -6,18 +6,13 @@ from PIL import Image, ImageDraw, ImageFont
 from FallenRobot import telethn as bot
 from FallenRobot.events import register
 
-Credit = "AnonymousBoy1025"
-
 
 @register(pattern="^/mmf ?(.*)")
 async def handler(event):
-
     if event.fwd_from:
-
         return
 
     if not event.reply_to_msg_id:
-
         await event.reply("Provide Some Text To Draw!")
 
         return
@@ -25,7 +20,6 @@ async def handler(event):
     reply_message = await event.get_reply_message()
 
     if not reply_message.media:
-
         await event.reply("```Reply to a image/sticker.```")
 
         return
@@ -34,16 +28,9 @@ async def handler(event):
 
     msg = await event.reply("```Memifying this image! ✊🏻 ```")
 
-    if "AnonymousBoy1025" in Credit:
-        pass
-
-    else:
-        await event.reply("This nigga removed credit line from code")
-
     text = str(event.pattern_match.group(1)).strip()
 
     if len(text) < 1:
-
         return await msg.reply("You might want to try `/mmf text`")
 
     meme = await drawText(file, text)
@@ -56,7 +43,6 @@ async def handler(event):
 
 
 async def drawText(image_path, text):
-
     img = Image.open(image_path)
 
     os.remove(image_path)
@@ -64,21 +50,17 @@ async def drawText(image_path, text):
     i_width, i_height = img.size
 
     if os.name == "nt":
-
         fnt = "ariel.ttf"
 
     else:
-
         fnt = "./FallenRobot/resources/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
     if ";" in text:
-
         upper_text, lower_text = text.split(";")
 
     else:
-
         upper_text = text
 
         lower_text = ""
@@ -88,9 +70,7 @@ async def drawText(image_path, text):
     current_h, pad = 10, 5
 
     if upper_text:
-
         for u_text in textwrap.wrap(upper_text, width=15):
-
             u_width, u_height = draw.textsize(u_text, font=m_font)
 
             draw.text(
@@ -131,9 +111,7 @@ async def drawText(image_path, text):
             current_h += u_height + pad
 
     if lower_text:
-
         for l_text in textwrap.wrap(lower_text, width=15):
-
             u_width, u_height = draw.textsize(l_text, font=m_font)
 
             draw.text(
