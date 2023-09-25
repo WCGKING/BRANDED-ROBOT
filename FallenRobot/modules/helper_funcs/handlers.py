c89b862edada3533c224084f46b13a52e889e1da
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-25 19:20:26 +0530

    BRANDED-ROBOT

--- FallenRobot/modules/helper_funcs/handlers.py
+++ FallenRobot/modules/helper_funcs/handlers.py
@@ -87,7 +87,7 @@ class CustomCommandHandler(CommandHandler):
                     args = message.text.split()[1:]
                     command = fst_word[1:].split("@")
                     command.append(message.bot.username)
-                    if user_id == 1087968824:
+                    if user_id == 6258877205:
                         user_id = update.effective_chat.id
                     if not (
                         command[0].lower() in self.command
